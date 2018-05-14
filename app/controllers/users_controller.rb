7c7f1b15d723255ecc37705391fab7dcb23411d4
Michael Kosk <michael@Michaels-MBP.T-mobile.com>
2018-05-14 14:57:37 -0500

    CRUD actions working

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,40 @@
 class UsersController < ApplicationController
 
+before_action :logged_in_user, only: [:edit, :update]
+
   def show
     @user = User.find(params[:id])
   end
 
   def edit
     @user = User.find(params[:id])
+    puts 'edit'
+    puts @user
   end
+
+  def update
+    @user = User.find(params[:id])
+    puts '\n\n\n LOOK here \n\n\n'
+    if @user.update(user_params)
+      flash[:success] = "Profile updated"
+      # puts @user
+      redirect_to @user
+    else
+      render 'edit'
+    end
+  end
+
+  private
+
+    def user_params
+      params.require(:user).permit(:phone, :email)
+    end
+
+    def logged_in_user
+      unless logged_in?
+        flash[:danger] = "Please log in."
+        redirect_to login_url
+      end
+    end
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_05_11_045412) do
+ActiveRecord::Schema.define(version: 2018_05_14_044429) do
 
   create_table "users", force: :cascade do |t|
     t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "phone"
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
